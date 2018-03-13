0b9c8e0a1ffc381fa1c9fbd69680d3045170c058
Joel Chia <joelchia_power@hotmail.com>
2018-03-13 12:32:43 +0800

    Implement the ef api

--- helpers/deploymentplan-helper.js
+++ helpers/deploymentplan-helper.js
@@ -4,3 +4,73 @@ const Enemy = require('../models/enemy');
 const Firebase = require('../config/database');
 const deploymentPlanRef = Firebase.database().ref('DeploymentPlan');
 
+exports.readDeploymentPlan = (req, res) => {
+	deploymentPlanRef.once('value', snapshot => {
+		return res.json(snapshot);
+	});
+};
+
+
+exports.createDeploymentPlan = (req, res) => {	
+	const crisisCode = req.body.crisis_code.toUpperCase();	
+	const casualtySize = Number(req.body.casualty_size);
+	const date = moment().format('DD/MM/YYYY');
+	const time = moment().format(' HH:mm:ss');
+	
+	const crisis = new Crisis(crisisCode, casualtySize, date, time);
+
+	var planId = deploymentPlanRef.push().key;
+	
+	deploymentPlanRef.child(planId)
+		.set(crisis)
+		.then(() => {
+			const enemyName = req.body.enemy_name;
+			const enemySize = Number(req.body.enemy_size);
+			const enemyType = req.body.enemy_type.toUpperCase();
+			const coordinateX = Number(req.body.coordinate_x);
+			const coordinateY = Number(req.body.coordinate_y);
+			const affectArea = Number(req.body.affect_area);
+
+			const enemy = new Enemy(enemyName, enemySize, enemyType, coordinateX, coordinateY, affectArea);
+
+			deploymentPlanRef.child(planId).child('Enemy').child(enemyName)
+				.set(enemy)
+				.then(() => res.json({
+					success: true,
+					message: 'Deployment Plan Added Successfully'
+				}))
+				.catch(err => res.json({
+					success: false,
+					message: 'Deployment Plan Added Failed'
+				}));
+		})
+		.catch(err => res.json({
+			success: false,
+			message: 'Deployment Plan Added Failed'
+		}));
+};
+
+exports.deleteDeploymentPlan = (req, res) => {
+	const planId = req.params.plan_id;
+
+	deploymentPlanRef.child(planId)
+		.once('value', snapshot => {
+			if (snapshot.exists()) {
+				deploymentPlanRef.child(planId)
+					.remove()
+					.then(() => res.json({
+						success: true,
+						message: 'Deployment Plan Deleted Successfully'
+					}))
+					.catch(err => res.json({
+						success: false,
+						message: 'Deployment Plan Deleted Failed'
+					}));
+			} else {
+				return res.json({
+					success: false,
+					message: 'Invalid deployment plan id'
+				});	
+			}
+		});
+};
--- routes/ef-api.js
+++ routes/ef-api.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const deploymentPlanHelper = require('../helpers/deploymentplan-helper');
 
+router.route('/ef')
+	.get(deploymentPlanHelper.readDeploymentPlan)
+	.post(deploymentPlanHelper.createDeploymentPaln);
 
+router.route('/ef/:plan_id')
+	.delete(deploymentPlanHelper.deleteDeploymentPlan);
+
+module.exports = router;
+
+// Route 						| Method	| Description
+// ---------------------------------------------------------------------------
+// api/ef 		 	 			| GET 	 	| Read all deployment plan
+// api/ef 						| POST 	 	| Create new deployment plan
+// api/ef/:plan_id 				| DELETE 	| Delete deployment plan
